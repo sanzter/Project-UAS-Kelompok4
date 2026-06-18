e29536df5dfd03d99f79d66296429a8651ac6163
sanzter <peterkingjo38@gmail.com>
2026-06-18 11:38:57 +0700

    Ralat Route

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,7 @@ Route::middleware(['auth', 'admin'])
 
     Route::get('/dashboard',   [AdminController::class, 'dashboard'])->name('dashboard');
     Route::post('/kelas', [AdminController::class, 'storeKelas'])->name('store-kelas');
+    Route::post('/jadwal', [AdminController::class, 'storeJadwal'])->name('store-jadwal');
     Route::post('/siswa/{id}/reset-kelas', [\App\Http\Controllers\AdminController::class, 'resetKelasSiswa'])->name('siswa.reset-kelas');
     Route::get('/kelola-siswa', [\App\Http\Controllers\AdminController::class, 'kelolaSiswa'])->name('admin.kelola-siswa');
     Route::get('/permintaan-keluar', [AdminController::class, 'kelolaSiswa'])->name('permintaan-keluar');
